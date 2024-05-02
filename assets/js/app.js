23b129272b99ce20e0a0e7926f174764dab23059
Lucas Täkker <50625016+Sniperhund@users.noreply.github.com>
2024-05-02 13:08:46 +0200

    Pop up færdig (med mobil version)

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,3 +4,11 @@ const btns = document.getElementById("btns")
 menu.addEventListener("click", () => {
     btns.classList.toggle("active")
 })
+
+const popup = document.getElementById("popup")
+const popup_btn = document.getElementById("popup-btn")
+
+document.addEventListener("click", (event) => {
+    if (event.target.classList.contains("fa-xmark"))
+        popup.classList.add("active-popup")
+})
